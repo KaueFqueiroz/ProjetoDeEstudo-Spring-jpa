4f4253ee754aeeb3c4f61646b77fd8274fabaffc
kaue freitas <kauefreitasmxk@gmail.com>
2026-02-10 22:43:32 -0300

    Projeto Criado

---
+++ src/test/java/com/projeto/ProjetoDeEstudo/ProjetoDeEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ProjetoDeEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
