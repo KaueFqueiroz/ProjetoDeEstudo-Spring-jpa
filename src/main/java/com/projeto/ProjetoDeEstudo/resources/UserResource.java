0a7925cd6a724939117c855d6c8a21f3d41655b8
kaue freitas <kauefreitasmxk@gmail.com>
2026-02-19 20:15:57 -0300

    User entity and resource

---
+++ src/main/java/com/projeto/ProjetoDeEstudo/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.projeto.ProjetoDeEstudo.resources;
+
+import com.projeto.ProjetoDeEstudo.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findALL(){
+        User u = new User(1L, "Gabriel", "gabriel@gmail.com", "999999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
